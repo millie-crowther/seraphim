a5bc198a90a9fc9b14619dcf74a6568840aad295
millie <mbc15@ic.ac.uk>
2018-07-15 23:21:04 +0100

    added renderable class

---
+++ include/renderable.h
@@ -0,0 +1,23 @@
+#ifndef RENDERABLE_H
+#define RENDERABLE_H
+
+#include <glm/glm.hpp>
+#include "mesh.h"
+
+struct model_t {
+    glm::mat4 tf;
+    mesh_t * mesh;
+
+    model_t(glm::mat4 t, mesh_t * m){ tf = t; mesh = m; }
+};
+
+class renderable_t {
+public:
+    // virtual methods
+    virtual bool is_visible(){ return true; }
+
+    // abstract methods
+    virtual model_t get_model() = 0;
+};
+
+#endif
