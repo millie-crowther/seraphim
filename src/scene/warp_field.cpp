c1144c74fee35f33c473d6ca44e5b25d6bc8210f
Amelia Crowther <amelia@Amelias-iMac.local>
2019-01-30 15:47:12 +0000

    can add warp functions to field

--- src/scene/warp_field.cpp
+++ src/scene/warp_field.cpp
@@ -4,3 +4,8 @@ warp_field_t::warp_field_t(){
     resolution = default_resolution;
     size = default_size;
 }
+
+void
+warp_field_t::add_warp_function(const warp_func_t& psi){
+    psis.push_back(psi);
+}
