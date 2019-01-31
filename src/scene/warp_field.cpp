1541f620337549a1c3de85c97e00fa883b84d842
Amelia Crowther <amelia@Amelias-iMac.local>
2019-01-31 11:21:49 +0000

    wrote some setters

--- src/scene/warp_field.cpp
+++ src/scene/warp_field.cpp
@@ -9,3 +9,19 @@ void
 warp_field_t::add_warp_function(const warp_func_t& psi){
     psis.push_back(psi);
 }
+
+void
+warp_field_t::set_size(int size){
+    this->size = size;
+}
+
+void 
+warp_field_t::set_resolution(int resolution){
+    this->resolution = resolution;
+}
+
+vec3_t
+warp_field_t::operator()(const vec3_t& v){
+    // TODO
+    return vec3_t(); 
+}
