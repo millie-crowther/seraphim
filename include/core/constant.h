da620d057a665d4bd71576e151d7a8733a52bf53
Amelia Crowther <amelia@Amelias-iMac.local>
2019-02-05 16:18:28 +0000

    formatting because im obssessive

--- include/core/constant.h
+++ include/core/constant.h
@@ -6,13 +6,13 @@ namespace constant {
     constexpr double epsilon = 0.00001;
 
     // shortest time period recognised by the engine = 8.3 ms = 120 Hz
-    constexpr double iota = 0.00833333333;
+    constexpr double iota    = 0.00833333333333333333333;
 
     // relationship between a circle and its diameter
-    constexpr double pi = 3.141592653589793238462643;
+    constexpr double pi      = 3.14159265358979323846264;
 
     // render distance = 10km
-    constexpr double rho = 10000;
+    constexpr double rho     = 10000;
 }
 
 #endif
