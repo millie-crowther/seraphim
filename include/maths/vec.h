4b83919d2175e06e0a47b04fcd581b10d899a292
Amelia Crowther <amelia@Amelias-iMac.local>
2019-02-14 10:52:28 +0000

    slightly neater vec class

--- include/maths/vec.h
+++ include/maths/vec.h
@@ -37,6 +37,16 @@ public:
         return result;
     }
 
+    // cross product only defined on three dimensional vectors
+    typename std::enable_if<N == 3, vec3_t>::type
+    cross(const vec3_t & v) const {
+        return vec3_t(
+            xs[1] * v.xs[2] - xs[2] * v.xs[1],
+            xs[2] * v.xs[0] - xs[0] * v.xs[2],
+            xs[0] * v.xs[1] - xs[1] * v.xs[0]
+        );
+    }
+
     /*
        norms
     */
@@ -146,6 +156,10 @@ public:
     vec_t<N> operator/(double scale) const {
         return *this * (1.0f / scale);
     }
+
+    vec3_t operator%(const vec3_t & v) const {
+        return cross(v);
+    }
 };
 
 template <unsigned int N>
@@ -155,16 +169,7 @@ operator*(double scale, const vec_t<N>& v){
 }
 
 typedef vec_t<2> vec2_t;
+typedef vec_t<3> vec3_t;
 typedef vec_t<4> vec4_t;
-
-class vec3_t : public vec_t<3> {
-public:
-    vec3_t();
-    vec3_t(double x);
-    vec3_t(double x, double y, double z);
-
-    vec3_t cross(const vec3_t & v) const;
-    vec3_t operator%(const vec3_t & v) const;
-};
 
 #endif
