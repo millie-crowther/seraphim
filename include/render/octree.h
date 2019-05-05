24dde5f01aba0e994b2aecc29abf27d6d2c6d9dc
millie-crowther <amelia.g.crowther@gmail.com>
2019-05-05 21:52:06 +0100

    Working on octree structure def

--- include/render/octree.h
+++ include/render/octree.h
@@ -6,6 +6,17 @@
 
 #include "renderable.h"
 
+/*
+
+01234567 01234567 01234567 01234567
+
+B
+01234567 01234567 01234567 01234567
+
+B = is described by a brick
+
+*/
+
 class octree_t {
 private:
     std::vector<uint64_t> structure;
@@ -17,7 +28,8 @@ private:
     bool is_homogenous(const aabb_t & aabb, const std::vector<std::weak_ptr<renderable_t>> & renderables) const;
     bool is_leaf(const vec3_t & x, const vec3_t & camera, const aabb_t & aabb) const;
 
-    int get_node(const vec3_t & x) const;
+    int get_node(const vec3_t & x, aabb_t * aabb) const;
+    int get_node_helper(const vec3_t & x, int i, aabb_t * aabb) const;
 
     void request_helper(
         const vec3_t & x, const vec3_t & camera, 
