79a0eb85d360fc82d25ee7fcfb21f2fb9e9bb3e6
millie-crowther <amelia.g.crowther@gmail.com>
2019-05-09 23:38:23 +0100

    wrote intersects and contains impl

--- src/render/renderable.cpp
+++ src/render/renderable.cpp
@@ -9,3 +9,13 @@ bool
 renderable_t::is_visible() const { 
     return visible; 
 }
+
+bool
+renderable_t::intersects(const aabb_t & aabb) const {
+    return sdf(aabb.get_centre()) < aabb.get_upper_radius();
+}
+
+bool
+renderable_t::contains(const aabb_t & aabb){
+    return sdf(aabb.get_centre()) < -aabb.get_upper_radius();
+}
