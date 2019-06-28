4c93ffd125d04281a766195410dbe8ad80a348e6
millie <amelia.g.crowther@gmail.com>
2019-06-28 23:21:31 +0100

    added input and mouse classes

---
+++ include/input/input.h
@@ -0,0 +1,16 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "input/mouse.h"
+#include "input/keyboard.h"
+
+class input_t {
+private:
+    mouse_t mouse;
+    keyboard_t keyboard;
+    
+public:
+
+};
+
+#endif
---
+++ include/input/mouse.h
@@ -0,0 +1,14 @@
+#ifndef MOUSE_H
+#define MOUSE_H
+
+#include "maths/vec.h"
+
+class mouse_t {
+private:
+    vec2_t position;
+
+public:
+
+};
+
+#endif
