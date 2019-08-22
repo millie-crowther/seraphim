17a27a228bba0c85b8622a795f4dc409d954304f
millie <amelia.g.crowther@gmail.com>
2019-08-22 13:35:15 +0100

    about to re add culling

--- include/render/octree.h
+++ include/render/octree.h
@@ -12,7 +12,7 @@ class octree_node_t {
 private:
     class octree_data_t {
     private:
-        static constexpr uint8_t node_type_empty  = 3;
+        static constexpr uint8_t node_type_empty  = 1;
 
 
         uint32_t type;
