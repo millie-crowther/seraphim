a6d2b0dc11c388538d0e4a44c0993c4c225644a2
millie <amelia.g.crowther@gmail.com>
2020-03-10 13:06:51 +0000

    small mem opt

--- src/render/request_manager.cpp
+++ src/render/request_manager.cpp
@@ -64,18 +64,16 @@ request_manager_t::request_manager_t(
     vkUpdateDescriptorSets(device->get_device(), write_desc_sets.size(), write_desc_sets.data(), 0, nullptr);
 
     f32vec4_t bounds(-hyper::rho, -hyper::rho, -hyper::rho, 2 * hyper::rho);
-    std::vector<std::vector<octree_node_t>> root_nodes;
-    for (auto sdf : sdfs){
-        root_nodes.push_back(octree_node_t::create(bounds, sdf));
-    }
 
     std::vector<octree_node_t> initial_octree;
     initial_octree.resize(work_group_count[0] * work_group_count[1] * work_group_size);
 
     for (uint32_t i = 0; i < initial_octree.size(); i += work_group_size){
-        for (uint32_t j = 0; j < root_nodes.size(); j++){
+        for (uint32_t j = 0; j < sdfs.size(); j++){
+            std::vector<octree_node_t> root_node = octree_node_t::create(bounds, sdfs[j]);
+
             for (uint32_t k = 0; k < 8; k++){
-                initial_octree[i + j * 8 + k] = root_nodes[j][k];
+                initial_octree[i + j * 8 + k] = root_node[k];
             }
         }
     }
