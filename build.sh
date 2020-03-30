1d52cab22e6ae0dd7789ea1dca0b0a3b1ce888b6
millie <mbc15@ic.ac.uk>
2020-03-30 16:33:25 +0100

    changing it so its easier to substitute in substance lookup

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 cd build
 cmake .
 make 
-
-echo "Verifying fragment shader..."
-~/VulkanSDK/1.1.101.0/x86_64/bin/glslangValidator -V ../src/render/shader/shader.frag
-
-echo "Verifying compute shader..."
-~/VulkanSDK/1.1.101.0/x86_64/bin/glslangValidator -V ../src/render/shader/shader.comp
