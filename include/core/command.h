053033fa07c7e08ff6e6a0f938efecf82013f6b6
millie <mbc15@ic.ac.uk>
2020-05-03 14:14:05 +0100

    put some of command file in a separate source file

--- include/core/command.h
+++ include/core/command.h
@@ -4,6 +4,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <memory>
 #include <stdexcept>
 
 class command_buffer_t {
@@ -47,23 +48,9 @@ public:
         }
     } 
 
-    ~command_buffer_t(){
-        vkFreeCommandBuffers(device, command_pool, 1, &command_buffer);
-    }
+    ~command_buffer_t();
 
-    void submit(VkSemaphore wait_sema, VkSemaphore signal_sema, VkFence fence, VkPipelineStageFlags stage){
-        VkSubmitInfo submit_info = {};
-        submit_info.pWaitDstStageMask = &stage;
-        submit_info.commandBufferCount = 1;
-        submit_info.pCommandBuffers = &command_buffer;
-        
-        submit_info.waitSemaphoreCount = wait_sema == VK_NULL_HANDLE ? 0 : 1;
-        submit_info.pWaitSemaphores = &wait_sema;
-        submit_info.signalSemaphoreCount = signal_sema == VK_NULL_HANDLE ? 0 : 1;
-        submit_info.pSignalSemaphores = &signal_sema;
-
-        vkQueueSubmit(queue, 1, &submit_info, fence);
-    }
+    void submit(VkSemaphore wait_sema, VkSemaphore signal_sema, VkFence fence, VkPipelineStageFlags stage);
 };
 
 class command_pool_t {
@@ -73,24 +60,8 @@ private:
     VkQueue queue;
 
 public:
-    command_pool_t(VkDevice device, uint32_t queue_family){
-        this->device = device;
-
-        VkCommandPoolCreateInfo command_pool_info = {};
-        command_pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-        command_pool_info.queueFamilyIndex = queue_family;
-        command_pool_info.flags = 0;
-
-        if (vkCreateCommandPool(device, &command_pool_info, nullptr, &command_pool) != VK_SUCCESS){
-            throw std::runtime_error("Error: failed to create command pool.");
-        }    
-
-        vkGetDeviceQueue(device, queue_family, 0, &queue);
-    }
-
-    ~command_pool_t(){
-        vkDestroyCommandPool(device, command_pool, nullptr);
-    }
+    command_pool_t(VkDevice device, uint32_t queue_family);
+    ~command_pool_t();
 
     template<class F>
     std::shared_ptr<command_buffer_t> one_time_buffer(const F & f) const {
