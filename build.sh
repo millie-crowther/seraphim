9afa2c96b6c67c068fbc49b4044d818849dd4778
millie <mbc15@ic.ac.uk>
2020-05-18 10:43:58 +0100

    run.sh script aborts if build fails:wq

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cd build
-cmake .
-make 
+cmake . || exit 1
+make || exit 1
+exit 0
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 ./build.sh
+[ $? -eq 0 ]  || exit 1
 
 cd build
 # export VK_LAYER_PATH=/Users/amelia/vulkansdk-macos-1.1.92.1/macOS/etc/vulkan/explicit_layer.d
