8c72e3fc2b7fda0f26b5a6911e393baccfe4a1e8
millie <mbc15@ic.ac.uk>
2020-08-16 13:39:36 +0100

    working on scheduler

--- include/core/scheduler.h
+++ include/core/scheduler.h
@@ -3,20 +3,41 @@
 
 #include <chrono>
 #include <functional>
+#include <queue>
 
 namespace scheduler {
     using clock_t = std::chrono::high_resolution_clock;
 
     struct task_t {
+        typedef std::function<void()> function_t;
+
         clock_t::time_point t;
-        std::function<void()> f;
+        function_t f;
 
         struct comparator_t {
             bool operator()(const task_t & a, const task_t & b);
         };
     };
 
-    void schedule(clock_t::time_point t, std::function<void()> f);
+    namespace {
+        std::priority_queue<task_t, std::vector<task_t>, task_t::comparator_t> task_queue;
+    }
+
+    template<class F>
+    void schedule_at(clock_t::time_point t, const F & f){
+        task_queue.push({ t, task_t::function_t(f) });
+    }
+
+    template<class F>
+    void schedule_after(double delta, const F & f){
+        auto t = clock_t::now() + std::chrono::microseconds(static_cast<uint32_t>(1000000 * delta));
+        schedule_at(t, f);
+    }
+
+    template<class F>
+    void schedule(const F & f){
+        schedule_at(clock_t::now(), f);
+    }
 }
 
 #endif
--- src/core/scheduler.cpp
+++ src/core/scheduler.cpp
@@ -1,5 +1,2 @@
 #include "core/scheduler.h"
 
-#include <queue>
-
-std::priority_queue<scheduler::task_t, std::vector<scheduler::task_t>, scheduler::task_t::comparator_t> task_queue;
