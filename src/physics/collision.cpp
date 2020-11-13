f84587ababea0c38fa74309d5f5dcef107083398
millie <mbc15@ic.ac.uk>
2020-11-13 15:34:14 +0000

    check for non colliding contact

--- src/physics/collision.cpp
+++ src/physics/collision.cpp
@@ -47,6 +47,15 @@ srph::collision_correct(const collision_t & c){
     auto n_a = c.a->get_transform().get_rotation() * c.a->get_sdf()->normal(x_a);
     auto n_b = c.b->get_transform().get_rotation() * c.b->get_sdf()->normal(x_b);
 
+    double sm = c.a->get_mass() + c.b->get_mass();
+    for (auto m : { c.a, c.b }){
+        auto x = m->get_transform().to_local_space(c.x);
+        auto n = m->get_transform().get_rotation() * m->get_sdf()->normal(x);
+        
+        // extricate matters 
+        m->get_transform().translate(c.fx * n * (1 - m->get_mass() / sm));
+    }
+    
 //*    
     n_a = vec3_t(0, 1, 0);
     n_b = vec3_t(0, -1, 0);
@@ -60,46 +69,48 @@ srph::collision_correct(const collision_t & c){
     auto vr = c.b->get_velocity(c.x) - c.a->get_velocity(c.x);
  
     auto n = vec::normalise(n_a - n_b);
+   
+    auto vrn = vec::dot(vr, n);
+    if (vrn > -constant::epsilon){
+        return;
+    } 
+
+
     double jr = -(1.0 + CoR) * vec::dot(vr, n) / (
         1.0 / c.a->get_mass() + c.a->get_inverse_angular_mass(c.x, n) +
         1.0 / c.b->get_mass() + c.b->get_inverse_angular_mass(c.x, n)
     );
+   
     
+ 
     // update velocities accordingly
     c.a->apply_impulse_at(-jr * n, c.x);
     c.b->apply_impulse_at( jr * n, c.x);
-    
-    double sm = c.a->get_mass() + c.b->get_mass();
+   
     for (auto m : { c.a, c.b }){
         auto x = m->get_transform().to_local_space(c.x);
         auto n = m->get_transform().get_rotation() * m->get_sdf()->normal(x);
         
-        // extricate matters 
-        m->get_transform().translate(c.fx * n * (1 - m->get_mass() / sm));
-        
         // calculate frictional force
     
-        /*    
+        //*    
         auto mat = m->get_material(m->to_local_space(c.x));
-        
+ 
         double js = mat.static_friction * jr;
         double jd = mat.dynamic_friction * jr;
 
-        vec3_t fe = m->get_acceleration();
-        vec3_t vrfe = vr;
-        if (std::abs(vec::dot(vr, n)) < constant::epsilon){
-            vrfe = fe;
-        }
+        vec3_t fe = m->get_mass() * m->get_acceleration();
+        std::vector<vec3_t> ts = { vr, fe, vec3_t() };
+        
+        int ti = 0;
+        if (std::abs(vec::dot(vr, n)) < constant::epsilon           ) ti++;
+        if (std::abs(vec::dot(fe, n)) < constant::epsilon && ti == 1) ti++;
 
-        vec3_t t;
-        if (std::abs(vec::dot(vrfe, n)) > constant::epsilon){
-            t = vec::normalise(vrfe - vec::dot(vrfe, n) * n);         
-        }
+        vec3_t t = vec::normalise(ts[ti] - vec::dot(ts[ti], n) * n);
+    //    std::cout << "t = " << t << std::endl;
 
-        double k = jd;
-        if (m->get_mass() * vec::dot(vr, t) <= js){
-            k = m->get_mass() * vec::dot(vr, t);
-        }
+        auto mvrt = m->get_mass() * vec::dot(vr, t);
+        double k = mvrt <= js ? mvrt : jd;
 
         m->apply_impulse_at(-k * t, c.x);
     //*/
