5d1427f3070952d01dd292724defbbc5763bbd9c
millie <mbc15@ic.ac.uk>
2021-02-26 20:28:27 +0000

    added default com calc

--- src/maths/sdf/sdf.cpp
+++ src/maths/sdf/sdf.cpp
@@ -83,7 +83,7 @@ double srph_sdf_volume(srph_sdf * sdf){
         srph_random rng;
         srph_random_default_seed(&rng);
     
-        for (int i = 0; i < VOLUME_SAMPLES; i++){
+        while (hits < VOLUME_SAMPLES){
             vec3 x;
             srph_vec3_set(&x,
                 srph_random_f64_range(&rng, b->lower[0], b->upper[0]),
@@ -103,10 +103,34 @@ double srph_sdf_volume(srph_sdf * sdf){
 }
 
 vec3 srph_sdf_com(srph_sdf * sdf){
-    // TODO
-    vec3 zero;
-    srph_vec3_fill(&zero, 0.0);
-    return zero;    
+    if (!sdf->_is_com_valid){
+        vec3 com = srph_vec3_zero;
+        double hits = 0.0;
+     
+        srph_bound3 * b = srph_sdf_bound(sdf);
+        srph_random rng;
+        srph_random_default_seed(&rng);
+    
+        while (hits < VOLUME_SAMPLES){
+            vec3 x;
+            srph_vec3_set(&x,
+                srph_random_f64_range(&rng, b->lower[0], b->upper[0]),
+                srph_random_f64_range(&rng, b->lower[1], b->upper[1]),
+                srph_random_f64_range(&rng, b->lower[2], b->upper[2])
+            );
+
+            if (srph_sdf_contains(sdf, &x)){
+                srph_vec3_add(&com, &com, &x);
+                hits += 1.0;
+            }
+        }
+
+        srph_vec3_scale(&com, 1.0 / hits);
+        sdf->_com = com;
+        sdf->_is_com_valid = true;
+    }
+
+    return sdf->_com;    
 }
 
 srph::mat3_t srph_sdf_inertia_tensor(srph_sdf * sdf){
