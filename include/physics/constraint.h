df1027577f1cfc94e8c8f1e1b6de845568b980be
millie <mbc15@ic.ac.uk>
2021-03-13 17:20:58 +0000

    afsd

--- include/physics/constraint.h
+++ include/physics/constraint.h
@@ -2,27 +2,34 @@
 #define SERAPHIM_CONSTRAINT_H
 
 #include "core/array.h"
+#include "maths/vector.h"
 
 typedef struct srph_vertex {
-    vec3 * x;
-    vec3 d;
+    vec3 * _x_key;
+    vec3 x;
     vec3 v;
     double w;
 } srph_vertex;
 
-typedef double (*srph_constraint_func)(const srph_vertex ** xs, uint32_t n);
+typedef double (*srph_constraint_func)(const void * data, srph_vertex ** xs, uint32_t n);
 typedef void (*srph_constraint_derivative_func)(
-    vec3 * dC, const srph_vertex ** xs, uint32_t i, uint32_t n
+    const void * data, vec3 * dC, srph_vertex ** xs, uint32_t i, uint32_t n
 );
 
 typedef struct srph_constraint {
-    bool is_equality;
-    double stiffness;
-    srph_array vertex_pointers;
-
-    srph_constraint_func c_func;
-    srph_constraint_derivative_func dc_func;
+    bool _is_equality;
+    double _stiffness;
+    srph_array _vertex_pointers;
+    void * _data;
+    srph_constraint_func _c_func;
+    srph_constraint_derivative_func _dc_func;
 } srph_constraint;
+
+void srph_constraint_init(
+    srph_constraint * c, bool is_equality, double stiffness,
+    void * data, srph_constraint_func c_func, srph_constraint_derivative_func dc_func
+);
+void srph_constraint_destroy(srph_constraint * c);
 
 void srph_constraint_update(const srph_constraint * c, vec3 * dp, uint32_t i, double s); 
 double srph_constraint_scaling_factor(const srph_constraint * c);
--- src/physics/constraint.cpp
+++ src/physics/constraint.cpp
@@ -1,27 +1,51 @@
 #include "physics/constraint.h"
 
-double srph_constraint_scaling_factor(const srph_constraint * c){
-    srph_vertex ** xs = (srph_vertex **) srph_array_first(&c->vertex_pointers);
-    uint32_t n = c->vertex_pointers.size;    
+#include <stdlib.h>
 
-    if (n == 0){
+void srph_constraint_init(
+    srph_constraint * c, bool is_equality, double stiffness,
+    void * data, srph_constraint_func c_func, srph_constraint_derivative_func dc_func
+){
+    c->_is_equality = is_equality;
+    c->_stiffness = stiffness;
+    c->_data = data;
+    c->_c_func = c_func;
+    c->_dc_func = dc_func; 
+
+    srph_array_create(&c->_vertex_pointers, sizeof(srph_vertex *));
+}
+
+void srph_constraint_destroy(srph_constraint * c){
+    if (c != NULL){
+        srph_array_destroy(&c->_vertex_pointers);
+    }
+}
+
+double srph_constraint_scaling_factor(const srph_constraint * c){
+    if (srph_array_is_empty(&c->_vertex_pointers)){
         return 0.0;
     }
 
-    double s = c->c_func(xs, n);
+    srph_vertex ** xs = (srph_vertex **) srph_array_first(&c->_vertex_pointers);
+    uint32_t n = c->_vertex_pointers.size;    
+    double s = c->_c_func(c->_data, xs, n);
     double q = 0.0;
     vec3 dc;
     for (uint32_t i = 0; i < n; i++){
-        c->dc_func(&dc, xs, i, n);
-        q += srph_vec3_dot(&c, &c) * xs[i]->w;
+        c->_dc_func(c->_data, &dc, xs, i, n);
+        q += srph_vec3_dot(&dc, &dc) * xs[i]->w;
+    }
+
+    if (q == 0.0){
+        return 0.0;
     }
 
     return s / q;
 }
 
 void srph_constraint_update(const srph_constraint * c, vec3 * dp, uint32_t i, double s){
-    srph_vertex ** xs = (srph_vertex **) srph_array_first(&c->vertex_pointers);
-    uint32_t n = c->vertex_points.size;
-    dc_func(dp, xs, i, n);
+    srph_vertex ** xs = (srph_vertex **) srph_array_first(&c->_vertex_pointers);
+    uint32_t n = c->_vertex_pointers.size;
+    c->_dc_func(c->_data, dp, xs, i, n);
     srph_vec3_scale(dp, dp, -s * xs[i]->w);
 }
